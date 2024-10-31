e27ef6b06ba4f12f603e14ef6807107fc7383a3a
yuki <yuki.for.neutral@gmail.com>
2024-10-31 14:22:53 +0900

    Refactor Timer component to reset focus and rest times based on selected values; update comments for clarity

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,3 +1,5 @@
+// todo
+// nextjs15にアップデートする
 "use client";
 
 import { useState, useEffect } from "react";
@@ -8,7 +10,7 @@ export default function Timer() {
   const [focusTime, setFocusTime] = useState(1500); // 初期値は25分＝1500秒
   const [restTime, setRestTime] = useState(300); //初期値は５分=300秒
   const [isRunning, setIsRunning] = useState(false);
-  const [isFocus, setIsFocus] = useState(true); // 初期状態はfocusTimer
+  const [isFocus, setIsFocus] = useState(true); // 初期はfocusTimer
 
   useEffect(() => {
     let timer: NodeJS.Timeout;
@@ -16,32 +18,44 @@ export default function Timer() {
       if (isFocus && focusTime > 0) {
         timer = setInterval(() => {
           setFocusTime((prevTime) => prevTime - 1);
-        }, 1000); // 1秒ごとに実行
+        }, 1000); // 1秒(1s=1000ms)ごとに実行する
       } else if (!isFocus && restTime > 0) {
         timer = setInterval(() => {
           setRestTime((prevTime) => prevTime - 1);
-        }, 1000); // 1秒ごとに実行
+        }, 1000);
       } else if (isFocus && focusTime === 0) {
         setIsFocus(false);
-        setRestTime(300); // 休憩時間をリセット
-        setFocusTime(1500); // 集中時間をリセット
+        const restSelect = document.getElementById(
+          "restTime"
+        ) as HTMLSelectElement;
+        setRestTime(Number(restSelect.value)); // 選択された休憩時間にリセット
+        const focusSelect = document.getElementById(
+          "focusTime"
+        ) as HTMLSelectElement;
+        setFocusTime(Number(focusSelect.value)); // 選択された集中時間にリセット
       } else if (!isFocus && restTime === 0) {
         setIsFocus(true);
-        setFocusTime(1500); // 集中時間をリセット
-        setRestTime(300); // 休憩時間をリセット
+        const focusSelect = document.getElementById(
+          "focusTime"
+        ) as HTMLSelectElement;
+        setFocusTime(Number(focusSelect.value)); // 選択された集中時間にリセット
+        const restSelect = document.getElementById(
+          "restTime"
+        ) as HTMLSelectElement;
+        setRestTime(Number(restSelect.value)); // 選択された休憩時間にリセット
       }
     }
     return () => clearInterval(timer);
   }, [isRunning, isFocus, focusTime, restTime]);
 
   // 集中する時間を選択した時間に変更する関数
-  const handleFocusChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setFocusTime(Number(event.target.value));
-  };
+  function handleFocusChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    setFocusTime(Number(e.target.value));
+  }
   // 休憩する時間を選択した時間に変更する関数
-  const handleRestChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setRestTime(Number(event.target.value));
-  };
+  function handleRestChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    setRestTime(Number(e.target.value));
+  }
 
   // スタートボタンを押した時の処理
   function handleStart() {
@@ -54,7 +68,7 @@ export default function Timer() {
   }
 
   // リセットボタンを押した時の処理
-  const handleReset = () => {
+  function handleReset() {
     setIsRunning(false);
     const focusSelect = document.getElementById(
       "focusTime"
@@ -63,7 +77,7 @@ export default function Timer() {
     // 選択された時間（数字）にする
     setFocusTime(Number(focusSelect.value));
     setRestTime(Number(restSelect.value));
-  };
+  }
 
   // 時間をMM:SS形式にフォーマットする関数
   // 引数はseconds秒数、返り値はstring文字列
@@ -88,7 +102,8 @@ export default function Timer() {
           <select
             name="focus"
             id="focusTime"
-            value={focusTime}
+            defaultValue="1500"
+            //value={focusTime}を使うと、selectの値がなぜか３０分になってしまう
             onChange={handleFocusChange}>
             <option value="1800">30分</option>
             <option value="1500">25分</option>
@@ -104,7 +119,7 @@ export default function Timer() {
           <select
             name="rest"
             id="restTime"
-            value={restTime}
+            defaultValue="300"
             onChange={handleRestChange}>
             <option value="300">5分</option>
             <option value="600">10分</option>
